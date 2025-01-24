416f802c97d7f2458bd01181e155dbb5588cec1d
eecopilot <flytoeep@gmail.com>
2025-01-24 15:36:08 +0800

    fix: 修正 Tabbar 文件夹大小写

---
+++ src/layout/components/Tabbar/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tabbar">
+    <div class="tabbar-left">
+      <BreadCrumb />
+    </div>
+    <div class="tabbar-right">
+      <Setting />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BreadCrumb from './Breadcrumb/index.vue'
+import Setting from './Setting.vue'
+</script>
+
+<style lang="scss" scoped>
+.tabbar {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+  &-left {
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+  }
+  &-right {
+    display: flex;
+    align-items: center;
+    padding-right: 10px;
+  }
+}
+</style>
